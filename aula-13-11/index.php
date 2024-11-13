b4229fdb035955055dbc6afd722cf2afbc5e8d61
paulowisniewski <pgabrielwisniewski@gmail.com>
2024-11-13 16:33:09 -0300

    .

--- aula-13-11/index.php
+++ aula-13-11/index.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    <title>aula-13-11</title>
 </head>
 <body>
-    <h1>Teste</h1>
+    
+    <nav>
+        <ul>
+            <li><a href="http://ceteia.guanambi.ifbaiano.edu.br/">Ceteia</a></li>
+            <li><a href="https://www.ifbaiano.edu.br/unidades/guanambi/">IF Guanambi</a></li>
+            <li><a href="https://suap.ifbaiano.edu.br/">Suap</a></li>
+            <li><a href="https://github.com/psw1-2024/atividades/blob/main/paulo-atv01/style.css">GitHub</a></li>
+        </ul>
+    </nav>
+
 </body>
 </html>
